f2a50cb8cf85d90a2d2d591a40940c20159544e2
timjossund <timjossund@gmail.com>
2021-09-17 17:40:01 -0500

    animations

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -25,7 +25,7 @@ function RenderCampsite({campsite}) {
         );
     };
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
         if (comments) {
             return(
                 <div className="col-md-5 m-1">
@@ -41,7 +41,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                         )
                     })
                     }
-                    <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                    <CommentForm campsiteId={campsiteId} postComment={postComment} />
                 </div>
             );
         }
@@ -72,7 +72,7 @@ class CommentForm extends Component {
         // console.log('Current state is: ' + JSON.stringify(values));
         // alert('Current state is: ' + JSON.stringify(values));
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
@@ -169,7 +169,7 @@ function CampsiteInfo(props) {
                         <RenderCampsite campsite={props.campsite} />
                         <RenderComments 
                             comments={props.comments} 
-                            addComment={props.addComment}
+                            postComment={props.postComment}
                             campsiteId={props.campsite.id}
                         />
                     </div>
